1dcdb0fde542cc653016e61d3e149300634f8309
khaoula219 <158485882+khaoula219@users.noreply.github.com>
2024-02-08 15:40:51 +0100

    Update PersonService.java

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -46,6 +46,13 @@ public class PersonService {
 		p.setFirstName("webhook");
 		p.setLastName("testJenjinsJob");
 		persons.add(p);
+
+		p = new Person();
+		p.setId("6");
+		p.setAge(22);
+		p.setFirstName("webhook");
+		p.setLastName("testJenjinsJob");
+		persons.add(p);
 		
 	}
 	public Person getPerson(String id) {
